8583c0ec131134a19412a1baba48f7cffe340523
mehdialajhoury <m_alajhoury@stu-pstb.fr>
2025-10-01 20:59:24 +0200

    ajout Exercice 1 Lists

---
+++ ExercicesLists/exercice1.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+#1. Print the first element from the list.
+print(fruits[0])
+
+#2. Print the third element from the list.
+print(fruits[1])
+
+#3. Try accessing an element at an index that doesn't exist (e.g., the 6th element) and note what happens.
+#print(fruits[5])
+
+#4. Change the second fruit ("banana") to "blueberry".
+fruits[1]="blueberry"
+print(fruits)
