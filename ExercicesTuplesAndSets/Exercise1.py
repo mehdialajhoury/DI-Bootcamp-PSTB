a13e585103bcaf6b6c3f64241d0c6297637eafe6
mehdialajhoury <m_alajhoury@stu-pstb.fr>
2025-10-01 21:48:59 +0200

    ajout Exercices Tuples And Sets

---
+++ ExercicesTuplesAndSets/Exercise1.py
@@ -0,0 +1,11 @@
+tuple_fruits = ("banana","peach","strawberry")
+
+#Accesses the first fruit in the tuple.
+print(tuple_fruits[0])
+
+#Counts how many times a specific fruit appears in the tuple.
+
+print(tuple_fruits.count("peach"))
+
+#Finds the index of a specific fruit in the tuple.
+print(tuple_fruits.index("strawberry"))
---
+++ ExercicesTuplesAndSets/Exercise2.py
@@ -0,0 +1,15 @@
+s = {3, 7, 64}
+
+#Adds a new number to the set.
+s.add(5)
+print(s)
+
+#Removes all elements from the set.
+s.clear
+
+#Finds the common elements between two sets (use a set of your five favorite numbers and a set of prime numbers).
+
+s = {3, 7, 5, 28, 64}
+s2 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
+
+print(s.intersection(s2))
