1c435a7fe310c666d88bbabc63a3a497199bc067
mehdialajhoury <m_alajhoury@stu-pstb.fr>
2025-10-01 21:19:10 +0200

    ajout Exercices Iterating Over Lists And Strings

---
+++ ExercicesIteratingOverListsAndStrings/exercise1.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "cherry", "date"]
+
+#Write a for loop that iterates over the following list of fruits and prints each fruit on a new line:
+
+for fruit in fruits:
+    print(fruit)
---
+++ ExercicesIteratingOverListsAndStrings/exercise2.py
@@ -0,0 +1,8 @@
+#Write a while loop that counts from 1 to 5 and prints each number on a new line.
+
+#IMPORTANT! use a counter to make sure you will not enter an eternal loop like: count = 0 and increment one in each iteration of the while loop: counter +=1.
+
+count=1
+while count <6:
+    print(count)
+    count +=1
